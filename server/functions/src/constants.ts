bc11c56ce73ce1c3824958d9c12fcac24a2a3fb8
SuuSoJeat <suusojeat@gmail.com>
2019-06-28 20:21:54 +0700

    add constants file

---
+++ server/functions/src/constants.ts
@@ -0,0 +1,12 @@
+
+
+export const COL_USERS = 'users'
+export const COL_DASHBOARD = 'dashboards'
+
+export const DOC_BALANCES = 'balances'
+
+export const defaultDashboardBalances = {
+    total: 0,
+    totalLent: 0,
+    totalBorrowed: 0
+}
