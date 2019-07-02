9cf4b5ce2e6dae23d568ffa57cb5c9e434d503e4
SuuSoJeat <suusojeat@gmail.com>
2019-07-02 21:20:28 +0700

    New flow with Firestore

--- lib/ui/widgets/recent_record_list.dart
+++ lib/ui/widgets/recent_record_list.dart
@@ -2,74 +2,53 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:valoro/core/models/debt.dart';
-import 'package:valoro/core/viewmodels/widgets/recent_record_model.dart';
-import 'package:valoro/ui/views/base_widget.dart';
+import 'package:valoro/core/services/firestore_service.dart';
 import 'package:valoro/ui/widgets/debt_item.dart';
 
 class RecentRecordList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BaseWidget<RecentRecordModel>(
-      model: RecentRecordModel(
-        firestoreService: Provider.of(context),
-        uid: Provider.of<FirebaseUser>(context).uid,
-      ),
-      builder: (context, model, child) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            child,
-            StreamBuilder<List<Debt>>(
-              stream: model.recentDebtsStream(),
-              builder: (context, AsyncSnapshot<List<Debt>> snapshot) {
-                if (snapshot.hasError) {
-                  return Text("Error");
-                }
-                if (!snapshot.hasData) {
-                  return Center(
-                    child: Text("No Item."),
-                  );
-                }
-                return Column(
-                  children: snapshot.data
-                      .map((doc) => DebtItem(
-                            debt: doc,
-                            onTap: () {},
-                          ))
-                      .toList(),
-                );
-//                switch (snapshot.connectionState) {
-//                  case ConnectionState.done:
-//                    if (snapshot.hasError) {
-//                      return Text("Error");
-//                    }
-//                    return Text("Has data");
-//                  case ConnectionState.none:
-//                    return Container();
-//                  case ConnectionState.waiting:
-//                    return Container();
-//                  case ConnectionState.active:
-//                    return Container();
-//                }
-              },
-            )
-          ],
+    return StreamBuilder<List<Debt>>(
+      stream: Provider.of<FirestoreService>(context)
+          .getRecentDebts(Provider.of<FirebaseUser>(context).uid),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text(snapshot.error.toString()),
+          );
+        }
+        if (snapshot.data.isEmpty) {
+          return Container();
+        }
+        return Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 24.0,
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Text(
+                  "Recent Records",
+                  style: TextStyle(
+                    fontSize: 23.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+              ...snapshot.data
+                  .map((d) => DebtItem(
+                        debt: d,
+                        onTap: () {},
+                      ))
+                  .toList()
+            ],
+          ),
         );
       },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 24.0,
-          horizontal: 16.0,
-        ),
-        child: Text(
-          "Recent Records",
-          style: TextStyle(
-            fontSize: 23.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.black87,
-          ),
-        ),
-      ),
     );
   }
 }
