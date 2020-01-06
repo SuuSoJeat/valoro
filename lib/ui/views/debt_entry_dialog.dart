11d6e3affff057e4d8073f203bbfca5920b98ba5
SuuSoJeat <suusojeat@gmail.com>
2020-01-07 00:33:40 +0700

    form completed

--- lib/ui/views/debt_entry_dialog.dart
+++ lib/ui/views/debt_entry_dialog.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:valoro/ui/widgets/segmented_button.dart';
 import 'package:valoro/ui/widgets/segmented_button_form_field.dart';
@@ -17,12 +18,16 @@ class DebtEntryDialog extends StatefulWidget {
 
 class _DebtEntryDialogState extends State<DebtEntryDialog> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _typeAheadController = TextEditingController();
+  final TextEditingController debtorNameTextController =
+      TextEditingController();
+  var _scaffoldKey = GlobalKey<ScaffoldState>();
 
-  @override
-  void initState() {
-    super.initState();
-  }
+ 
+  String description;
+  String currency = 'USD';
+  double amount;
+  String badgeType;
+  DateTime date = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -57,23 +62,24 @@ class _DebtEntryDialogState extends State<DebtEntryDialog> {
                     SizedBox(
                       width: 16,
                     ),
-                    _typeAheadController.text.length > 0
+                    debtorNameTextController.text.length > 0
                         ? InputChip(
                             avatar: CircleAvatar(
-                              child: Text("${_typeAheadController.text[0]}"),
+                              child:
+                                  Text("${debtorNameTextController.text[0]}"),
                             ),
-                            label: Text('${_typeAheadController.text}'),
+                            label: Text('${debtorNameTextController.text}'),
                             deleteIcon: Icon(Icons.close),
                             onDeleted: () {
                               setState(() {
-                                _typeAheadController.text = '';
+                                debtorNameTextController.text = '';
                               });
                             },
                           )
                         : Flexible(
                             child: TypeAheadFormField(
                               textFieldConfiguration: TextFieldConfiguration(
-                                  controller: _typeAheadController),
+                                  controller: debtorNameTextController),
                               itemBuilder: (context, Contact contact) {
                                 return ListTile(
                                   title: Text("${contact.displayName}"),
@@ -82,7 +88,7 @@ class _DebtEntryDialogState extends State<DebtEntryDialog> {
                               suggestionsCallback: contactsCallback,
                               onSuggestionSelected: (Contact contact) {
                                 setState(() {
-                                  _typeAheadController.text =
+                                  debtorNameTextController.text =
                                       contact.displayName;
                                 });
                               },
@@ -115,10 +121,18 @@ class _DebtEntryDialogState extends State<DebtEntryDialog> {
                     Flexible(
                       child: TextFormField(
                         decoration: InputDecoration(
-                            hintText: "Enter a description",
-                            hintStyle: GoogleFonts.roboto(
-                                textStyle:
-                                    TextStyle(color: Colors.blueGrey[900]))),
+                          hintText: "Enter a description",
+                          hintStyle: GoogleFonts.roboto(
+                            textStyle: TextStyle(
+                              color: Colors.blueGrey[900],
+                            ),
+                          ),
+                        ),
+                        onSaved: (desc) {
+                          setState(() {
+                            description = desc;
+                          });
+                        },
                       ),
                     )
                   ],
@@ -160,6 +174,9 @@ class _DebtEntryDialogState extends State<DebtEntryDialog> {
                           ),
                         ),
                         keyboardType: TextInputType.number,
+                        onSaved: (inputAmount) {
+                          amount = double.tryParse(inputAmount);
+                        },
                       ),
                     )
                   ],
@@ -173,7 +190,7 @@ class _DebtEntryDialogState extends State<DebtEntryDialog> {
                     BadgeOption.borrowing: true
                   },
                   onSaved: (option) {
-                    print(option);
+                    badgeType = option;
                   },
                 ),
               )
@@ -192,9 +209,34 @@ class _DebtEntryDialogState extends State<DebtEntryDialog> {
               children: <Widget>[
                 FlatButton.icon(
                   icon: Icon(Icons.date_range),
-                  onPressed: () {},
+                  onPressed: () {
+                    // DatePicker.showDateTimePicker(context);
+                    // DatePicker.showDatePicker(
+                    //   context,
+                    //   showTitleActions: true,
+                    //   onChanged: (date) {
+                    //     print('change $date');
+                    //   },
+                    //   onConfirm: (date) {
+                    //     print('confirm $date');
+                    //   },
+                    //   currentTime: DateTime.now(),
+                    //   locale: LocaleType.en,
+                    // );
+                    var datePicking = showDatePicker(
+                      context: context,
+                      initialDate: date,
+                      firstDate: DateTime(2018),
+                      lastDate: DateTime(2030),
+                    );
+                    datePicking.then((chosenDate) => {
+                          setState(() {
+                            if (chosenDate != null) date = chosenDate;
+                          })
+                        });
+                  },
                   label: Text(
-                    "Today",
+                    "${DateFormat('MMM dd, yyyy').format(date)}",
                     style: GoogleFonts.roboto(),
                   ),
                 ),
@@ -249,6 +291,9 @@ class _DebtEntryDialogState extends State<DebtEntryDialog> {
   }
 
   void actionSavePressed() {
-    _formKey.currentState.save();
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      print('${debtorNameTextController.text} $description $currency $amount $badgeType $date');
+    }
   }
 }
