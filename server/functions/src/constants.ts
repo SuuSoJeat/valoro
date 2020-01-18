71ad7b31a6d3d6e78f57dbb4b280cf125ce139c9
SuuSoJeat <suusojeat@gmail.com>
2020-01-19 01:01:40 +0700

    add createNewDebt callable

--- server/functions/src/constants.ts
+++ server/functions/src/constants.ts
@@ -1,12 +1,13 @@
 
 
 export const COL_USERS = 'users'
-export const COL_DASHBOARD = 'dashboards'
+export const COL_DASHBOARD = 'dashboard'
+export const COL_DEBTS = 'debts'
+export const COL_RECORDS = 'records'
 
 export const DOC_BALANCES = 'balances'
 
 export const defaultDashboardBalances = {
-    total: 0,
     totalLent: 0,
     totalBorrowed: 0
 }
